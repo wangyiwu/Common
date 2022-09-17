c2f2c3bcb6caeb420432d8f77ba00bca7596374b
Phùng Xuân Vượng <vuongdingo@gmail.com>
2022-09-17 15:48:38 +0700

    @

---
+++ src/Common/String/StringExtensions.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace Common.String.Extensions
+{
+    public static class StringExtensions
+    {
+        public static string GetTenantKey(this string path, string key)
+        {
+            var regexPattern = "([a-zA-Z0-9]+):([a-zA-Z0-9])+";
+
+            var match = Regex.Match(path, regexPattern);
+
+            if (!match.Success)
+            {
+                throw new Exception();
+            }
+
+            foreach(Group group in match.Groups)
+            {
+                if (group.Value.Contains(key) && group.Value.Split(":").Length == 2)
+                {
+                    return group.Value.Split(":")[1];
+                }
+            }
+
+            return "";
+        }
+    }
+}
