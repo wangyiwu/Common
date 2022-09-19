08e06bd1e33431ef71f497daaeae54f82eb10ab4
Phùng Xuân Vượng <vuongdingo@gmail.com>
2022-09-19 07:43:17 +0700

    add-qr-code

---
+++ src/Common/QRCode/QRCodeExtension.cs
@@ -0,0 +1,19 @@
+﻿using QRCoder;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Common.QRCode
+{
+    public static class QRCodeExtension
+    {
+        public static byte[] GenerateQrCode(this string textToEncode)
+        {
+            QRCodeGenerator qrGenerator = new QRCodeGenerator();
+            QRCodeData qrCodeData = qrGenerator.CreateQrCode(textToEncode, QRCodeGenerator.ECCLevel.Q);
+            BitmapByteQRCode qrCode = new BitmapByteQRCode(qrCodeData);
+            byte[] qrCodeAsBitmapByteArr = qrCode.GetGraphic(20);
+            return qrCodeAsBitmapByteArr;
+        }
+    }
+}
